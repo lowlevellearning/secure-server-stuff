49deee47e7aff8497d45ff1eee54a0581e4d3e3d
gianzellweger <gian.zellweger@ict-scouts.ch>
2022-11-26 22:57:37 +0100

    Used quotes instead of angle-brackets and a compiler flag to include secrets.h into hacked.c

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sudo apt install gcc-multilib
-gcc -o hacked hacked.c secrets.c -fno-stack-protector -I$(pwd) -m32 -no-pie
+gcc -o hacked hacked.c secrets.c -fno-stack-protector -m32 -no-pie
--- hacked.c
+++ hacked.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <secrets.h>
+#include "secrets.h"
 
 void debug() 
 {
